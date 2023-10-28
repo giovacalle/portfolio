607c336451d7816b7710c2591b22e89190c96f72
Gc <giovacalle.dev@gmail.com>
2023-10-28 22:18:30 +0200

    feat: migration to v3 :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-
-import solidJs from "@astrojs/solid-js";
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), solidJs()]
+  integrations: [tailwind(), solidJs()]
 });
