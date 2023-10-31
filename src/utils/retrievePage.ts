4f70395a36174654c50947d2c6d7064d02f6c1cb
Gc <giovacalle.dev@gmail.com>
2023-10-31 23:53:48 +0100

    fix: change build cuz vercel gets mad (slash)

--- src/utils/retrievePage.ts
+++ src/utils/retrievePage.ts
@@ -5,17 +5,17 @@ type Page = astroHTML.JSX.IntrinsicElements['div'] & {
 };
 export const retrievePage = (page: string): Page | undefined => {
   const pages: { [key: string]: Page } = {
-    '/me': {
+    '/me.html': {
       style: {
         backgroundImage: `url(${avatar})`
       },
       class: 'bg-cover bg-bottom bg-no-repeat'
     },
-    '/techs': {
+    '/techs.html': {
       icon: 'mdi:code',
       class: 'bg-gradient-to-r from-violet-400 to-red-500'
     },
-    '/projects': {
+    '/projects.html': {
       icon: 'mdi:draw',
       class: 'bg-gradient-to-r from-green to-blue-500'
     }
