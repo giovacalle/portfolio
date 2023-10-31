aad446076dda9e47f0098c5e69faee8dcde4e280
Gc <giovacalle.dev@gmail.com>
2023-10-31 23:53:48 +0100

    fix: change build cuz vercel gets mad (slash)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  trailingSlash: 'never',
   build: {
     format: 'file'
   }
