a411cbd1d0c7ec0b31880f1e7d766fe6e76618e8
Gc <giovacalle.dev@gmail.com>
2023-11-04 18:10:27 +0100

    feat: come back to directory build conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,5 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
-  trailingSlash: 'never',
-  build: {
-    format: 'file'
-  }
+  trailingSlash: 'never'
 });
