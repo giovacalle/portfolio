08981b57b96a4569e3298d1829a2083ca80e138c
Gc <giovacalle.dev@gmail.com>
2023-11-04 21:40:38 +0100

    feat: add sitemap :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  site: 'https://giova.dev',
+  integrations: [tailwind(), solidJs(), sitemap()],
   trailingSlash: 'always' // so vercel doesn't complain :)
 });
